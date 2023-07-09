2eaa4ab3eec5d97bd25d40a27fa1bc6f8a224a0e
cork03 <tsubasa.t1119@gmail.com>
2023-07-09 20:04:48 +0900

    stream処理有効化

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
 resource "aws_dynamodb_table" "dynamo_streams" {
   //partition key
-  hash_key = "UserId"
-  name = "dynamo_streams"
-  read_capacity = 1
+  hash_key       = "UserId"
+  name           = "dynamo_streams"
+  read_capacity  = 1
   write_capacity = 1
+
+  // ストリームの有効化
+  stream_enabled = true
+  //  更新されたItemから渡すデータを設定する
+  stream_view_type = "NEW_IMAGE"
 
   // テーブルカラム
   attribute {
